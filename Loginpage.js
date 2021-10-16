3ad99fe8814aa576e7d354c44bf4e5c137f603c1
SaiGaneshCH <90609468+SaiGaneshCH@users.noreply.github.com>
2021-10-16 16:36:42 +0530

    Add files via upload

---
+++ Loginpage.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/Menu';
+import Button from '@mui/material/Button';
+import Home from './Home';  
+import Register from './Register';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety'; 
+import { BrowserRouter as Router,Link,Route,Switch } from 'react-router-dom';
+export class Loginpage extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+    
+    Home = e => {
+        e.preventDefault();
+        this.render({Home})
+    }
+    
+    render() {
+        const { values , handleChange } = this.props;
+        return (
+
+            <MuiThemeProvider>
+            <React.Fragment>
+                <AppBar  title="Login to your account" IconButton='HealthAndSafetyIcon'  />
+            
+                <TextField
+                hintText = "Enter your User Name"
+                floatingLabelText="User Name"
+                onChange = { handleChange('userName') } 
+                defaultValue = {values.userName}
+                />
+                    <br/>
+            <TextField
+                hintText = "Enter your Email"
+                floatingLabelText="or Enter your email"
+                onChange = { handleChange('email') } 
+                defaultValue = {values.email}
+                />
+  
+                <br/>
+                <TextField
+                hintText = "Enter your Password"
+                floatingLabelText="Your password"
+                onChange = { handleChange('password') } 
+                defaultValue = {values.password}
+                />
+        <br/>
+        <br/>
+         <Button startIcon={<HealthAndSafetyIcon/>}
+         color="primary"
+              variant="contained"
+              onClick={Home}>Login</Button>
+        <br/>
+        <br/>
+        <p>Do not have an account!......Register here 👇👇</p>                          
+        
+        <Router>
+<Switch>
+<Route path="/Register" component={Register} />     
+ <Link onClick={this.continue}>Register</Link>
+</Switch>
+</Router>  
+              
+            </React.Fragment>
+            </MuiThemeProvider>
+ 
+        )
+    }
+}
+
+
+
+export default Loginpage
---
+++ Register.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import Button from '@mui/material/Button';
+import './Register.css'
+import { ClassNames } from '@emotion/react';
+export class Register extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    }
+    render() {
+        const { values , handleChange } = this.props;
+        return (
+           <div div className='Reg'>
+            <MuiThemeProvider>
+            <React.Fragment>
+                <AppBar title="Enter Your Details" />
+                <TextField
+                hintText = "Enter your First Name"
+                floatingLabelText="First Name"
+                onChange = { handleChange('firstName') } 
+                defaultValue = {values.firstName}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your Last Name"
+                floatingLabelText="Last Name"
+                onChange = { handleChange('lastName') } 
+                defaultValue = {values.lastName}
+                />
+            <br/>
+            <TextField
+                hintText = "Enter your Email"
+                floatingLabelText="sample@gmail.com"
+                onChange = { handleChange('email') } 
+                defaultValue = {values.email}
+                />
+  <br/>
+                <TextField
+                hintText = "Enter your Mobile Number"
+                floatingLabelText="Mobile Number"
+                onChange = { handleChange('mobileNumber') } 
+                defaultValue = {values.mobileNumber}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your Age"
+                floatingLabelText="Age"
+                onChange = { handleChange('age') } 
+                defaultValue = {values.age}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your City"
+                floatingLabelText="City"
+                onChange = { handleChange('city') } 
+                defaultValue = {values.city}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your Address"
+                floatingLabelText="Address"
+                onChange = { handleChange('address') } 
+                defaultValue = {values.address}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your Password"
+                floatingLabelText="Enter strong Password"
+                onChange = { handleChange('password') } 
+                defaultValue = {values.password}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your Password again"
+                floatingLabelText="Confirm Password"
+                onChange = { handleChange('password') } 
+                defaultValue = {values.password}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your Occupation"
+                floatingLabelText="Occupation"
+                onChange = { handleChange('occupation') } 
+                defaultValue = {values.occupation}
+                />
+                <br/>
+                <TextField
+                hintText = "Enter your bio"
+                floatingLabelText="Your Bio"
+                onChange = { handleChange('bio') } 
+                defaultValue = {values.bio}
+                />
+                <br/>
+                <br/>
+                <Button color="primary"                                      
+              variant="contained"
+              onClick={this.continue}>Continue</Button>      <Button color="primary"
+              variant="contained"
+              onClick={this.back}>Back</Button>
+                
+            </React.Fragment>
+            </MuiThemeProvider>
+            </div>
+        )
+    }
+}
+
+
+export default Register
