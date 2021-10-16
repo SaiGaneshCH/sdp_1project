d5588508c9e0e53ac0b1274c17ab7b19fdb0c075
SaiGaneshCH <90609468+SaiGaneshCH@users.noreply.github.com>
2021-10-16 16:32:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Userform from './Components/Userform';
+
+function App() {
+  return (
+    <div className="App">
+      <Userform />
+    </div>
+  );
+}
+
+
+export default App;
