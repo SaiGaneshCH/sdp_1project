3fd1a9f7b6a8564a2f0284151a97fc87e3cc9861
SaiGaneshCH <90609468+SaiGaneshCH@users.noreply.github.com>
2021-10-18 21:09:12 +0530

    Update Loginpage.js

--- Loginpage.js
+++ Loginpage.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui/Menu';
 import Button from '@mui/material/Button';
 import Home from './Home';  
 import Register from './Register';
 import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety'; 
-import { BrowserRouter as Router,Link,Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router,Link,Route,Switch, render } from 'react-router-dom';
 export class Loginpage extends Component {
     continue = e => {
         e.preventDefault();
         this.props.nextStep();
     }
-    
-    Home = e => {
-        e.preventDefault();
-        this.render({Home})
-    }
+  to = e =>{
+      e.preventDefault();
+      this.props.keepStep();
+  }
     
     render() {
         const { values , handleChange } = this.props;
         return (
-
+<>
             <MuiThemeProvider>
             <React.Fragment>
                 <AppBar  title="Login to your account" IconButton='HealthAndSafetyIcon'  />
@@ -39,8 +35,8 @@ export class Loginpage extends Component {
             <TextField
                 hintText = "Enter your Email"
                 floatingLabelText="or Enter your email"
-                onChange = { handleChange('email') } 
-                defaultValue = {values.email}
+                onChange = { handleChange('email1') } 
+                defaultValue = {values.email1}
                 />
   
                 <br/>
@@ -55,7 +51,7 @@ export class Loginpage extends Component {
          <Button startIcon={<HealthAndSafetyIcon/>}
          color="primary"
               variant="contained"
-              onClick={Home}>Login</Button>
+              onClick={this.to}>Login</Button>
         <br/>
         <br/>
         <p>Do not have an account!......Register here 👇👇</p>                          
@@ -69,7 +65,7 @@ export class Loginpage extends Component {
               
             </React.Fragment>
             </MuiThemeProvider>
- 
+ </>
         )
     }
 }
