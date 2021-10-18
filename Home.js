e01cc4358a2372ddf32b81574f6f24156517b19d
SaiGaneshCH <90609468+SaiGaneshCH@users.noreply.github.com>
2021-10-18 21:12:14 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,17 @@
+import { MuiThemeProvider } from 'material-ui/styles';
+import React, { Component } from 'react'
+
+export class Home extends Component {
+ 
+  render() {
+    return (
+      <div className='Home'>
+        <h1>
+          This is a sample Home page
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Home
