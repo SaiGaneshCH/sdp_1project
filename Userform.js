07334d91a0dbbefa88c387adefb544c058bb2ab6
SaiGaneshCH <90609468+SaiGaneshCH@users.noreply.github.com>
2021-10-18 21:11:32 +0530

    Update Userform.js

--- Userform.js
+++ Userform.js
@@ -1,12 +1,19 @@
 import  React, { Component } from 'react'
 import Register from './Register'
 import Loginpage from './Loginpage'
+import Confirm from './Confirm'
+import Home from './Home'
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety'; 
+import { Button } from '@mui/material'
+import Success from './Success'
+import Login from './register_success_Login'
 export class Userform extends Component {
     state = {
         step: 1,
         firstName: '',
         lastName: '',
-        email: '',
+        email1: '',
+        email2:'',
         occupation: '',
         city: '',
         mobileNumber: '',
@@ -14,6 +21,7 @@ export class Userform extends Component {
         userName: '',
         age: '',
         password: '',
+        confirmpassword: '',
         address: '',
     }
     // To register page
@@ -30,24 +38,36 @@ export class Userform extends Component {
             step: step-1
         });
     }
+    //To set step
+    keepStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 5
+        });
+    }
+    
     handleChange = input => e => {
         this.setState({ [input]: e.target.value});
     };
+  
     render() {
         const { step } = this.state;
-        const { firstName,lastName, email,occupation,city,mobileNumber,bio,userName,age,password,address } = this.state;
-    const values = { firstName,lastName, email,occupation,city,mobileNumber,bio,userName,age,password,address }
+        const { firstName,lastName, email1,email2,occupation,city,mobileNumber,bio,userName,age,password,address,confirmpassword } = this.state;
+    const values = { firstName,lastName, email1,email2,occupation,city,mobileNumber,bio,userName,age,password,address,confirmpassword }
   
 
     switch(step){ 
+        
         case 1:
         return(
             <Loginpage
             nextStep={this.nextStep}
+            keepStep={this.keepStep}
             handleChange={this.handleChange}
             values={values}
             />
             )
+         
         case 2:
             return(
                 <Register
@@ -57,13 +77,31 @@ export class Userform extends Component {
                 values={values}
                 />
                 )
-                case 2: 
-                return <h1>PersonalDetails</h1>
-            
                 case 3: 
-            return <h1>Confirm</h1>
+                return <Confirm
+                nextStep={this.nextStep}
+                prevStep={this.prevStep}
+                handleChange={this.handleChange}
+                values={values}
+                />
+                
             case 4: 
-            return <h1>Success</h1>
+            return <Success
+            nextStep={this.nextStep}
+            values={values}/>
+            case 5:
+                return(
+                    <Login
+                    nextStep={this.nextStep}
+                    handleChange={this.handleChange}
+                    values={values}
+                    />
+                    )
+             case 6:
+                return(
+                    <Home/>
+                         )
+            
         }    
    
     }
